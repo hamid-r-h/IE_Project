a09b75aac48dae82738f068b8abf9364bf8bf5c8
Ali nooran <angenesis04@gmail.com>
2022-07-02 22:20:39 +0430

    favorites page completed

--- src/component/Favorites.js
+++ src/component/Favorites.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Favorites.module.css";
 import torob from "../img/torob.jpg";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+const authAxios = axios.create({
+  headers: {
+    Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+  },
+});
 
 const Favorites = () => {
+  const [favs, setFavs] = useState();
+
+  const removeFav = (id) => {
+    authAxios
+      .delete(`http://localhost:9000/api/user/favorites/${id}`)
+      .then()
+      .catch((err) => console.log(err));
+      setFavs(favs.filter(a => a._id !== id));
+  };
+
+  useEffect(() => {
+    authAxios
+      .get("http://localhost:9000/api/user/favorites")
+      .then((res) => {
+        setFavs(res.data.favorites);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <div className={`${style.header}`}>محبوب ها</div>
-      <div className={`${style.card_container}`}>
-        <div className={style.card}>
-            <img src={torob} />
-            <Link to={`/products`}><h3>S20 fe</h3></Link>
-            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است،</p>
-            <button>حذف از محبوب ها</button>
-        </div>
-        <div className={style.card}>card 2</div>
-        <div className={style.card}>card 3</div>
-        <div className={style.card}>card 4</div>
-        <div className={style.card}>card 5</div>
-      </div>
+      {favs === undefined
+        ? ""
+        : favs.map((item) => (
+            <div className={`${style.card_container}`} key={item._id}>
+              <div className={style.card}>
+                <img src={item.image} />
+                {/* <Link to={`/products`}> */}
+                <h3>{item.name}</h3>
+                {/* </Link> */}
+                <p>{item.description.slice(0, 20) + "..."}</p>
+                <button onClick={() => removeFav(item._id)}>
+                  حذف از محبوب ها
+                </button>
+              </div>
+            </div>
+          ))}
     </>
   );
 };
