a334e7fdbca5ba8075174f1c8a14d2577433a873
Ali nooran <angenesis04@gmail.com>
2022-07-02 23:56:44 +0430

    logout option added to profile page

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -3,28 +3,35 @@ import { Link } from "react-router-dom";
 import style from "./Profile.module.css";
 
 const Profile = () => {
+  const logout = () => {
+    sessionStorage.removeItem("token");
+    console.log('hi');
+    window.location.href = "http://localhost:3000";
+  };
+
   return (
     <>
-      {/* <div className="ui grid container"> */}
-        <div className={`${style.header}`}>نمایه ها</div>
-        <ul className={style.ul}>
-          <li>
-            <Link to={`/profile/edit`}>
-              <div>ویرایش اطلاعات</div>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/profile/favorites`}>
-              <div>محبوب ها</div>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/profile/shops`}>
-              <div>لیست فروشگاه ها</div>
-            </Link>
-          </li>
-        </ul>
-      {/* </div> */}
+      <div className={`${style.header}`}>نمایه ها</div>
+      <ul className={style.ul}>
+        <li>
+          <Link to={`/profile/edit`}>
+            <div>ویرایش اطلاعات</div>
+          </Link>
+        </li>
+        <li>
+          <Link to={`/profile/favorites`}>
+            <div>محبوب ها</div>
+          </Link>
+        </li>
+        <li>
+          <Link to={`/profile/shops`}>
+            <div>لیست فروشگاه ها</div>
+          </Link>
+        </li>
+        <li onClick={logout}>
+          <div>خروج از حساب</div>
+        </li>
+      </ul>
     </>
   );
 };
