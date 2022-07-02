951b7fa83688937a7b5d3c8bb42d223065b33a8c
Ali nooran <angenesis04@gmail.com>
2022-07-03 00:01:17 +0430

    add type and description to reports of shops in database model

--- server/model/shop.js
+++ server/model/shop.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const shopSchema = mongoose.Schema({
     name: String,
-    reports: [String]
+    reports: [{
+        type: String,
+        description: String
+    }]
 });
 
 const Shop = mongoose.model('shop', shopSchema);
